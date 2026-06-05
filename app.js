d0deab3e79f81d2ceeaa8ff385599a3216a5fc71
parham <mrprpy@gmail.com>
2026-06-06 02:18:41 +0330

    Fix: clean Jest exit and conditional server start

--- app.js
+++ app.js
@@ -26,8 +26,10 @@ app.get('/counter', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 module.exports = app; // exported so we can test it
